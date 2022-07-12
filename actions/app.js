212195e5dbb0c0f130d16bc1d59446603a30c4f5
Lloyd <lloyd.appdeture@gmail.com>
2022-07-12 14:30:28 +0800

    updated

--- actions/app.js
+++
@@ -1,6 +0,0 @@
-// all actions here or create new actions and export it to index.js
-const appActions = {
-  
-}
-
-export default appActions
--- actions/index.js
+++
@@ -1,7 +0,0 @@
-import appActions from './app'
-import utilsActions from './utils'
-
-export default {
-  ...appActions,
-  ...utilsActions
-}
