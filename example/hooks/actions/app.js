5634167eb33754626aa2180ba9deb06aefc4c659
Lloyd <lloyd.appdeture@gmail.com>
2022-07-14 17:38:06 +0800

    minor update

--- example/hooks/actions/app.js
+++ example/hooks/actions/app.js
@@ -1,8 +1,6 @@
-import { async } from "q";
-
 const appActions = {
-  addItem: async (store, item) => {
-    await store.setState({ items: [...store.state.items, item] })
+  addItem: (store, item) => {
+    store.setState({ items: [...store.state.items, item] })
   }
 }
 
